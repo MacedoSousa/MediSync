3ffb3ebf46827a81f5595d30070c5253049baefd
Pedro Macedo <pedro.macedo@atmtec.com.br>
2025-09-01 21:16:55 -0300

    Ajustando projeto.

--- frontend/src/pages/HomePage.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import PagePlaceholder from '../components/UI/PagePlaceholder';
-
-function HomePage() {
-  return <PagePlaceholder title="Home" />;
-}
-
-export default HomePage;
